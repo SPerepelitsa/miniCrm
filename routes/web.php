787be4cb55e4029c71f4d211e6b15ff6f779ff46
SPerepelitsa <sergei.poltava@gmail.com>
2019-03-02 17:38:30 +0200

    AdminLTE package added

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('admin/dashboard');
+})->middleware('auth');
+
 Auth::routes(['register' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
